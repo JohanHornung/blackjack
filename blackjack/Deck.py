0de28ccc9a98dbeddfb50babb15219a547a7a298
Johan <johanhornung93@gmail.com>
2021-02-11 19:48:20 +0100

    method location changed

--- blackjack/Deck.py
+++ blackjack/Deck.py
@@ -47,6 +47,10 @@ class Deck:
     # def shuffle(self):
     #     pass
 
+    # method which deals the first for cards out
+    def deal(self):
+        pass
+
     # method which lets the player hit a card (randomly)
     def hit(self, person):
         # handling the low prob case when the whole deck is empty
@@ -99,31 +103,14 @@ class Deck:
     # method for tracking cards which have been hit/taken
     # def track(self):
     #     pass
-    
-    # methods which splits his deck
-    def split(self, player_hand:list, bet:int):
-        """
-        Even if the condition should always be true, an error handling
-        procedure has been done anyways.
-        """
-        # make sure that the player has 2 cards (only 2!)
-        self.condition = len(player_hand) == 2
-        if not self.condition:
-            raise ValueError("Joeur a besoin de 2 cartes!") 
-        
-    
-    # method which lets the player double his deck
-    def double(self):
-        pass
-    
-        
 
+    def blackjack(self, player_hand): # optional arg
+        
 
     # print method which prints the current deck/hand
     def displayHands(self):
         return "Player´s Hand: ",self.player_hand, "Dealer´s Hand: ", self.dealer_hand
 
 # Deck instance
-game = Deck([], [])
+# game = Deck([], [])
 
-condition = game.split([1]) 
--- blackjack/Game.py
+++ blackjack/Game.py
@@ -11,5 +11,39 @@ the outcomes (6 different them).
 - it will return the new budget of the player (later more for data-analysis) 
 """
 
-class Game(Deck):
-    pass
+class Game:
+    def __init__(self, bet):
+        self.bet = bet
+        # for conditonal treatement later on
+        self.split = False 
+        self.double = False
+    
+    # method which lets the player double his game
+    def double(self): # bet arg optional
+        pass
+    
+    # methods which splits his deck
+    def split(self, player_hand:list): # bet arg optional
+        self.split = True
+        """
+        Even if the condition should always be true, an error handling
+        procedure has been done anyways.
+        """
+        # make sure that the player has 2 cards (only 2!) and that they have the same value
+        self.condition = (len(player_hand)) == 2 and (player_hand[0]["value"] == player_hand[1]["value"])
+        if not self.condition:
+            if len(player_hand) != 2:
+                raise ValueError("Joeur a besoin de 2 cartes!") 
+            else:
+                raise ValueError("Joueur n'a pas les meme valeurs") 
+
+        self.bets = [self.bet // 2, self.bet // 2]
+        self.first_hand = player_hand[0]
+        self.second_hand = player_hand[1]
+        """
+        Basically, the following steps are to play each hand itself, the second
+        one gets played if the first one is overbought or when he passed.          
+        """
+
+x = Game(50)
+x.split([4, 4])
