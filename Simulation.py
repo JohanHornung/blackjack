2bdc22912b127c4aafce22e0219581923d108850
Johan <johanhornung93@gmail.com>
2021-05-02 19:20:11 +0200

    main simulation steps done

--- Simulation.py
+++ Simulation.py
@@ -1,9 +1,10 @@
 import numpy as np # for advanced ds (arrays, matrices, ...)
 import pandas as pd # data management tools 
-import random
 import matplotlib.pyplot as plt # mathematical tool
 import seaborn as sbn # data visualisation
 from Game import * # Game class for the blackjack
+import random
+
 
 class Simulation:
     def __init__(self, stacks, mode="smart", num_players=1, num_decks=1):
@@ -18,7 +19,7 @@ class Simulation:
         self.game = Game() # 20000 staks, 1 player, 1 deck
 
     # method which plays the simulated games 1 by 1
-    def simulation(self):
+    def create(self):
         for _ in range(self.stacks): # iteration through the stacks
             
             self.blackjack = set(['A',10]) # {10, "A"} --> all possible blackjacks
@@ -62,23 +63,26 @@ class Simulation:
                             curr_player_results[0, player] = 1 # win
                         
                         else:
-                            """
-                            OLD: Now, the player has the choice to either hit or stay, if we suppose that
-                            the probability for hitting is the same as for staying, we can at each turn 
-                            generate a random value between 0 and 1, if this value is higher than 0.5, 
-                            the player hits, else we stay (do nothing). We additionally check for busts.
-                            NEW: As a random hit leads to the same chance of hitting when the player has 20
-                            than standing so a new method has to be written.
-                            """
-                            while ((self.game.total_up(self.players_hands[player]) <= 11) and  
-                                (self.game.total_up(self.players_hands[player]) != 21)):
-                                self.players_hands[player].append(self.cards.pop(0))
-                                
-                                # check for bust again
-                                if self.game.total_up(self.players_hands[player]) > 21:
-                                    curr_player_results[0,player] = -1 # loss
-                                    break # game over for this player
-                
+                            # simulating both types 
+                            if (self.mode == "smart"): # smart simulation
+                                while ((self.game.total_up(self.players_hands[player]) <= 11) and  
+                                    (self.game.total_up(self.players_hands[player]) != 21)):
+                                    self.players_hands[player].append(self.cards.pop(0))
+                                    
+                                    # check for bust again
+                                    if self.game.total_up(self.players_hands[player]) > 21:
+                                        curr_player_results[0,player] = -1 # loss
+                                        break # game over for this player
+                            elif (self.mode == "smart"): # random simulation
+                                while ((random.random() >= 0.5) and # 'coin flip' method  
+                                    (self.game.total_up(self.players_hands[player]) != 21)):
+                                    self.players_hands[player].append(self.cards.pop(0))
+                                    
+                                    # check for bust again
+                                    if self.game.total_up(self.players_hands[player]) > 21:
+                                        curr_player_results[0,player] = -1 # loss
+                                        break # game over for this player
+
                 # dealer hits until 17 or more
                 while self.game.total_up(self.dealer_hand) < 17:    
                     self.dealer_hand.append(self.cards.pop(0))
@@ -139,26 +143,26 @@ class Simulation:
         # defining our data model attributes for modelisationisation
         
         # dataframe object (table)
-        self.model_df = pd.DataFrame()
+        self.df_model = pd.DataFrame()
         # entry with all the dealer cards
-        self.model_df["dealer_card"] = self.dealer_card_result
+        self.df_model["dealer_card"] = self.dealer_card_result
         # entry with all total sums of the player for each game
-        self.model_df["player_total_sums"] = [self.game.total_up(sum[0][0:2]) for sum in self.player_card_result]
+        self.df_model["player_total_sums"] = [self.game.total_up(sum[0][0:2]) for sum in self.player_card_result]
         # results of each game (1, 0 or -1)
-        self.model_df["results"] = [result[0] for result in self.player_results]
+        self.df_model["results"] = [result[0] for result in self.player_results]
 
-        # print(self.model_df["dealer_card"])
-        # print(self.model_df["player_total_sums"])
-        # print(self.model_df["results"])
+        # print(self.df_model["dealer_card"])
+        # print(self.df_model["player_total_sums"])
+        # print(self.df_model["results"])
 
         self.lost = [] # either lost or won (a tie is evaluated as a win)  
-        for result in self.model_df['results']:
+        for result in self.df_model['results']:
             if result == -1: # dealer win
                 self.lost.append(1)
             else:
                 self.lost.append(0)
         # adding the results to a new df attribute
-        self.model_df['lost'] = self.lost
+        self.df_model['lost'] = self.lost
         
         # making an array to know if the player had an ace or not
         self.player_has_ace = []
@@ -170,25 +174,25 @@ class Simulation:
                 self.player_has_ace.append(0) # == false
         
         # adding the results to a new df attribute
-        self.model_df['player_has_ace'] = self.player_has_ace
+        self.df_model['player_has_ace'] = self.player_has_ace
 
         # array for replacing the ace by his numerical value
         dealer_card_val = []
-        for card in self.model_df['dealer_card']:
+        for card in self.df_model['dealer_card']:
             if card == "A":
                 dealer_card_val.append(11)
             else:
                 dealer_card_val.append(card)
 
         # adding the results to a new df attribute
-        self.model_df['dealer_card_val'] = dealer_card_val
+        self.df_model['dealer_card_val'] = dealer_card_val
         
-        return self.model_df
+        return self.df_model
     
     # method which returns a table of the player probability of having an ace or not
     def has_ace(self):
-        self.aces = (self.model_df.groupby(by ='player_has_ace').sum()['lost'] / 
-                    self.model_df.groupby(by='player_has_ace').count()['lost'])
+        self.aces = (self.df_model.groupby(by ='player_has_ace').sum()['lost'] / 
+                    self.df_model.groupby(by='player_has_ace').count()['lost'])
         return self.aces
     
     """
@@ -196,11 +200,11 @@ class Simulation:
     dealers first cards.
     NOTE: the player always decides based on the FIRST dealer card
     """
-    def first_dealer_card_impact(self, df_model: object, xlabel: str, ylabel: str, save_to="images"):
+    def first_dealer_card_impact(self, xlabel: str, ylabel: str, save_to="images"):
         # print(pd.DataFrame(self.player_results)[0].value_counts())
         # grouping data
-        self.data = round(1 - (df_model.groupby(by='dealer_card').sum()['lost'] /           
-        df_model.groupby(by='dealer_card').count()['lost']), 3)
+        self.data = round(1 - (self.df_model.groupby(by='dealer_card').sum()['lost'] /           
+        self.df_model.groupby(by='dealer_card').count()['lost']), 3)
         # print(self.data.index)
         # plotting axes for x and y
         _, ax = plt.subplots(figsize=(10, 6))
@@ -211,18 +215,18 @@ class Simulation:
         ax.set_ylabel(ylabel, fontsize=15)
         plt.tight_layout()
         # saving
-        plt.savefig(fname=f'{save_to}/dealer_first_card_probs', dpi=250)
+        plt.savefig(fname=f'{save_to}/{self.mode}_dealer_card_impact', dpi=200)
 
     """
     Method which creates a barplot with the probability of winning (or tie) for all the 
     total players hand values.
     """
-    def player_value_impact(self, df_model: object, x_label: str, y_label: str, save_to="images"):
+    def player_value_impact(self, x_label: str, y_label: str, save_to="images"):
         # pretty much the same procedure is done as for the dealers cards
-        self.data = 1 - (df_model.groupby(by='player_total_sums').sum()['lost'] /
-                    df_model.groupby(by='player_total_sums').count()['lost'])
+        self.data = 1 - (self.df_model.groupby(by='player_total_sums').sum()['lost'] /
+                    self.df_model.groupby(by='player_total_sums').count()['lost'])
         # print(self.data)  
-        _, self.axis = plt.subplots(figsize=(10, 6))
+        _, self.axis = plt.subplots(figsize=(10, 6),)
         # obviously the prob of a win or tie is 1 so we ignore it
         self.axis = sbn.barplot(x=self.data[:-1].index,
                          y=self.data[:-1].values)
@@ -230,13 +234,13 @@ class Simulation:
         self.axis.set_ylabel(y_label, fontsize=15)
 
         plt.tight_layout()
-        plt.savefig(fname=f'{save_to}/player_hand_probs', dpi=150)
+        plt.savefig(fname=f'{save_to}/{self.mode}_player_value_impact', dpi=200)
         # print(pd.DataFrame(player_results)[0].value_counts())
 
     # method which saves a heatmap of the probabilities of the player/dealer cards
-    def heatmap(self, df_model: object, x_label: str, y_label: str, save_to="images"):
+    def heatmap(self, x_label: str, y_label: str, save_to="images"):
         # creating table without the rows were the player´s sum is above 21
-        self.pivot_data = self.model_df[self.model_df['player_total_sums'] != 21]
+        self.pivot_data = self.df_model[self.df_model['player_total_sums'] != 21]
         # creating a table with all the collected losses for a specific dealer first card value (for each player sum)
         self.losses_pivot = pd.pivot_table(self.pivot_data, values='lost', 
                                       index=['dealer_card_val'], # row
@@ -253,77 +257,68 @@ class Simulation:
         # create plot
         _, self.axis = plt.subplots(figsize=(16, 8))
         # creazte heatmap
-        sbn.heatmap(self.heat_data, square=False, cmap="PiYG");
+        sbn.heatmap(self.heat_data, square=False, cmap="GnBu") # cmap for color palette
         # define axis´s
         self.axis.set_xlabel(x_label, fontsize=16)
         self.axis.set_ylabel(y_label, fontsize=16)
 
-        plt.savefig(fname=f'{save_to}/heat_map_{self.mode}', dpi=150)
+        plt.savefig(fname=f'{save_to}/{self.mode}_heat_map', dpi=200)
 
-        def model_comparison(self):
+    # method which returns a barmap comparing 2 types of data frame game results
+    def model_comparison(self, first_df, second_df, save_to="images"):
+        # helper func
+        def setup(df_model: object, column: str, xLabel: str):
             pass
+        
+        # for the player hand values
+        # collecting prob. results for both df´s
+        self.first_data = 1 - (first_df.groupby(by='player_total_sums').sum()['lost'] /                  
+                                first_df.groupby(by='player_total_sums').count()['lost'])
+        self.second_data = 1 - (second_df.groupby(by='player_total_sums').sum()['lost'] /                   
+                                second_df.groupby(by='player_total_sums').count()['lost'])
+        # creating new dataframe with both new data
+        self.data = pd.DataFrame()
+        self.data[f"{first_df}"] = self.first_data[:-1] # ignore 21 probabilities
+        self.data[f"{second_df}"] = self.second_data[:-1] # ignore 21 probabilities
+        # create plot
+        _, self.axix = plt.subplots(figsize=(12,6))
+        # creating both bars for each value with labels
+        self.axix.bar(x=self.data.index-0.2, height=self.data[f"{first_df}"].values, color='blue', width=0.4, label='smart')
+        self.axix.bar(x=self.data.index+0.2, height=self.data[f"{second_df}"].values, color='red', width=0.4, label='random')
+        # set labels
+        self.axix.set_xlabel("Player's Hand Value", fontsize=16)
+        self.axix.set_ylabel("Probability of Tie or Win", fontsize=16)
+        
+        plt.xticks(np.arange(4, 21, 1.0)) # sets the steps between each bar
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(fname=f'{save_to}/{self.mode}_player_hand_comparison', dpi=200)
+        
+        # for the dealers first card 
+        # collecting prob. results for both df´s
+        self.first_data = 1 - (first_df.groupby(by='dealer_card_val').sum()['lost'] /                  
+                                first_df.groupby(by='dealer_card_val').count()['lost'])
+        self.second_data = 1 - (second_df.groupby(by='dealer_card_val').sum()['lost'] /                   
+                                second_df.groupby(by='dealer_card_val').count()['lost'])
+        # creating new dataframe 
+        self.data = pd.DataFrame()
+        # 
+        self.data[f"{first_df}"] = self.first_data
+        self.data[f"{second_df}"] = self.second_data
+        # create plot
+        _, self.axix = plt.subplots(figsize=(12,6))
+        # creating both bars for each value with labels
+        self.axix.bar(x=self.data.index-0.2, height=self.data[f"{first_df}"].values, color='blue', width=0.4, label='smart')
+        self.axix.bar(x=self.data.index+0.2, height=self.data[f"{second_df}"].values, color='red', width=0.4, label='random')
+        # set labels
+        self.axix.set_xlabel("Dealers first card", fontsize=16)
+        self.axix.set_ylabel("Probability of Tie or Win", fontsize=16)
+        
+        plt.xticks(np.arange(2, 11, 1.0)) # sets the steps between each bar
+        plt.legend()
+        plt.tight_layout()
+        plt.savefig(fname=f'{save_to}/{self.mode}_dealer_card_comparison', dpi=200)
 
-
-
-
-simulation = Simulation(1000, "smart")
-simulation.simulation()
-simulation.evaluate()
-simulation.modelisation()
-simulation.heatmap(simulation.model_df, "Player's hand value", "Dealer´s first card")
-# simulation.first_dealer_card_impact(simulation.model_df, "Dealer's first card", "Probability of Tie or Win")
-# simulation.player_value_impact(simulation.model_df, "Player's hand value", "Probability of a tie or win")
-# print(simulation.has_ace())
-# print(simulation.model_df)
-
-
-
-
-
-
-
-
-# data_smart = 1 - (model_df_smart.groupby(by='dealer_card_num').sum()['lose'] /                  
-# model_df_smart.groupby(by='dealer_card_num').count()['lose'])
-# data_random = 1 - (model_df.groupby(by='dealer_card_num').sum()['lose'] /                   
-# model_df.groupby(by='dealer_card_num').count()['lose'])
-
-# data = pd.DataFrame()
-# data['smart'] = data_smart
-# data['random'] = data_random
-
-# fig, ax = plt.subplots(figsize=(12,6))
-# ax.bar(x=data.index-0.2, height=data['smart'].values, color='blue', width=0.4, label='Smart')
-# ax.bar(x=data.index+0.2, height=data['random'].values, color='red', width=0.4, label='Coin Flip')
-# ax.set_xlabel("Dealer's Card",fontsize=16)
-# ax.set_ylabel("Probability of Tie or Win",fontsize=16)
-# plt.xticks(np.arange(2, 12, 1.0))
-
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig(fname='dealer_card_probs_smart', dpi=150)
-
-
-
-
-
-# data_smart = 1 - (model_df_smart.groupby(by='player_total_initial').sum()['lose'] /                  model_df_smart.groupby(by='player_total_initial').count()['lose'])
-# data_random = 1 - (model_df.groupby(by='player_total_initial').sum()['lose'] /                   model_df.groupby(by='player_total_initial').count()['lose'])
-
-# data = pd.DataFrame()
-# data['smart'] = data_smart[:-1]
-# data['random'] = data_random[:-1]
-
-# fig, ax = plt.subplots(figsize=(12,6))
-# ax.bar(x=data.index-0.2, height=data['smart'].values, color='blue', width=0.4, label='Smart')
-# ax.bar(x=data.index+0.2, height=data['random'].values, color='red', width=0.4, label='Coin Flip')
-# ax.set_xlabel("Player's Hand Value",fontsize=16)
-# ax.set_ylabel("Probability of Tie or Win",fontsize=16)
-# plt.xticks(np.arange(4, 21, 1.0))
-
-# plt.legend()
-# plt.tight_layout()
-# plt.savefig(fname='player_hand_probs_smart', dpi=150)
-
-
-
+    # method which exports main information about the simulations
+    def export_headers(self):
+        pass
---
+++ main.py
@@ -0,0 +1,89 @@
+# main file of the project where everything is beeing executed
+from Game import *
+from Simulation import *
+import time as t
+
+# main func
+def main(stacks):
+    start = t.time()
+    # creating a smart and random model/simulation
+    smart_simulation, random_simulation = Simulation(stacks, "smart"), Simulation(stacks, "random")
+    # simulating
+    smart_simulation.create()
+    random_simulation.create()
+    # outcome statistics
+    smart_simulation.evaluate()
+    random_simulation.evaluate()
+    # modelizing data frames
+    smart_simulation.modelisation()
+    random_simulation.modelisation()
+    # visualizing
+    smart_simulation.player_value_impact("Player´s value", "Probability of win or tie")
+    random_simulation.player_value_impact("Player´s value", "Probability of win or tie")
+    smart_simulation.first_dealer_card_impact("Dealers first card", "Probability of win or tie")
+    random_simulation.first_dealer_card_impact("Dealers first card", "Probability of win or tie")
+    smart_simulation.heatmap("Player´s hand value", "Dealer´s card")
+    random_simulation.heatmap("Player´s hand value", "Dealer´s card")
+    # comparing
+    smart_simulation.model_comparison(smart_simulation.df_model, random_simulation.df_model)
+    # exporting headers
+    smart_simulation.export_headers()
+    random_simulation.export_headers()
+
+    end = t.time()
+    print("Time elapsed: " + str(end - start))
+    print("\n")
+    # print("Smart simulation: ", smart_simulation.stats)
+    # print("\n")
+    # print("Random simulation: ", random_simulation.stats)
+    
+    # print(smart_simulation.df_model)
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    stacks = int(input("How many stacks?\n"))
+    main(stacks)
